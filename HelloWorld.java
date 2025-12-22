9770e3ab953cf5eeff01f26ffe98ae9b56c59f0a
Karina paudel <karinapaudel6@gmail.com>
2025-12-22 12:14:08 +0545

    Updated message in HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Hello, Jenkins!"); // Change the message
+    System.out.println("Hello Jenkins Devops!");
   }
 }
